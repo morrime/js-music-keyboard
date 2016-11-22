4f2daa83a0325a3da56fde0d2ed2ba35778b72bc
morrime <marymorrison15@gmail.com>
2016-11-21 16:47:22 -0800

    met baseline and refactored. done probably

--- noise.js
+++ noise.js
@@ -1,6 +1,16 @@
+var loadPlay = function(sound){
+  sound.load();
+  sound.play();
+};
+
 $(document).ready( function() {
   $('.note').on("click", function() {
-    alert('Clicked on letter!');
+    var selection = $(this).html();
+    var sound = $("#" + selection + "Audio")[0];
   });
 
+  $("body").on("keydown", function(event) {
+    var sound = $("#" + event.key + "Audio")[0];
+    loadPlay(sound);
+  });
 });
